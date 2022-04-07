f8b7266d8b34d40022702a3e956dc41653ab4a8d
ilkan atli <atliilkan@gmail.com>
2022-04-07 23:28:15 +0300

    Vücut Kitle İndeksi Hesaplama

---
+++ src/vucutKitleIndeksi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class vucutKitleIndeksi {
+    public static void main(String[] args) {
+        //Değişkenleri alalım
+
+        double kilo, boy, vucutKitleIndeksi;
+
+        //Kullanıcıdan verileri alalım
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Boyunuzu(M) giriniz: ");
+        boy = input.nextDouble();
+
+        System.out.print("Kilonuzu(KG) giriniz: ");
+        kilo = input.nextInt();
+
+        vucutKitleIndeksi = kilo / (boy * boy);
+        System.out.println("Vucut kitle indeksi: " + vucutKitleIndeksi);
+
+    }
+}
