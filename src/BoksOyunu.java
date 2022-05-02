c65dcfe99f903961ee508ca0f7d935472ea5f743
İLKAN ATLI <89912656+ilkanatli@users.noreply.github.com>
2022-05-02 11:18:53 +0300

    Delete BoksOyunu.java

--- src/BoksOyunu.java
+++
@@ -1,9 +0,0 @@
-public class BoksOyunu {
-    public static void main(String[] args) {
-        Fighter f1 = new Fighter("Muhammed Ali", 10, 120, 100, 30);
-        Fighter f2 = new Fighter("Joe Frazier", 20, 85, 85, 40);
-
-        Match match = new Match(f1, f2, 85, 100);
-        match.run();
-    }
-}
