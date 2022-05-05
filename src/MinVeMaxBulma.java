1a61cb91cc9df8647eed780d2fa5f9ce5be74e1c
ilkan atli <atliilkan@gmail.com>
2022-05-05 22:57:18 +0300

    Girilen Sayıdan Küçük En Yakın Sayıyı Ve En Büyük En Yakın Sayıyı Bulan Program

---
+++ src/MinVeMaxBulma.java
@@ -0,0 +1,22 @@
+public class MinVeMaxBulma {
+    public static void main(String[] args) {
+        int[] list = {56, 34, 1, 8, 101, -2, -33};
+        // min = list[0] , min < list[i] ? min = list[i] : ''
+        // max = list[0] , max > list[i] ? max = list[i] : ''
+
+        int min = list[0];
+        int max = list[0];
+
+        for (int i : list) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println("Minimum Deger: " + min);
+        System.out.println("Maximum Deger: " + max);
+    }
+}
+
