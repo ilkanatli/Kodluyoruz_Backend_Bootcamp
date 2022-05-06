7173b0cca076c35cfd28390f22b2e440657a5060
ilkan atli <atliilkan@gmail.com>
2022-05-07 00:22:26 +0300

    Dizideki Elemanların Frekansını Bulma

---
+++ src/DizidekiTekrarEdenSayilar.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+public class DizidekiTekrarEdenSayilar {
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return  true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int [] list = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1, 2, 10, 4, 6, 4, 6};
+        int [] duplicate = new int[list.length];
+        int startIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i!=j) && list[i]==list[j] && list[i]% 2==0) {
+                        if (!isFind(duplicate, list[i])) {
+                            duplicate[startIndex++] = list[i];
+                        }
+                    break;
+                }
+            }
+        }
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+    }
+}
